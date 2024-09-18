018a36cd932749547a82c31a6b93406e12c8a817
Quan Pham <qmpham2019@gmail.com>
2024-09-18 15:53:11 -0400

    Implemented processor for Lenovo processing
    Note that, for now, only the Lenovo invoice will take the processed
    data from the `LenovoProcessor`. All other invoices will take the
    data from `AddInstituteProcessor`. This is due to the processors
    adding new columns. This odd code design will be removed once invoices
    gain the feature to filter out their exported columns.

--- process_report/invoices/lenovo_invoice.py
+++ process_report/invoices/lenovo_invoice.py
@@ -6,7 +6,6 @@ import process_report.invoices.invoice as invoice
 @dataclass
 class LenovoInvoice(invoice.Invoice):
     LENOVO_SU_TYPES = ["OpenShift GPUA100SXM4", "OpenStack GPUA100SXM4"]
-    SU_CHARGE_MULTIPLIER = 1
 
     export_columns_list = [
         invoice.INVOICE_DATE_FIELD,
@@ -18,12 +17,6 @@ class LenovoInvoice(invoice.Invoice):
         "Charge",
     ]
     exported_columns_map = {invoice.SU_HOURS_FIELD: "SU Hours"}
-
-    def _prepare(self):
-        self.data["SU Charge"] = self.SU_CHARGE_MULTIPLIER
-
-    def _process(self):
-        self.data["Charge"] = self.data[invoice.SU_HOURS_FIELD] * self.data["SU Charge"]
 
     def _prepare_export(self):
         self.data = self.data[
